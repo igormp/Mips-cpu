fbf7b30401f7565b21a6231a199e89ca9c549b77
Igor Moura <imp2@cin.ufpe.br>
2018-06-05 01:37:26 -0300

    removed unecessary files

--- ShiftLeft2.sv
+++
@@ -1,8 +0,0 @@
-module ShiftLeft2(In, Out);
-
-input [31:0] In;
-output [31:0] Out;
-
-assign Out = In << 2;
-
-endmodule 
--- ShiftLeft2Conc.sv
+++
@@ -1,9 +0,0 @@
-module ShiftLeft2Conc(In, Out);
-
-input [25:0] In;
-output [27:0] Out;
-
-assign Out[27:2] = In[25:0];
-assign Out[1:0] = 2'd0;
-
-endmodule 
